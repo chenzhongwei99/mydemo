09c8c4d216049ce517351640b7c5ed8b1b712ddc
陈忠伟.信息技术中心.无线系统部 <chenzhongwei@yolo24.com>
2017-03-03 17:44:12 +0800

    test demo

---
+++ demo-service/src/main/java/com/demo/controller/Test.java
@@ -0,0 +1,13 @@
+package com.demo.controller;
+
+/**
+ * Created by chenzhongwei on 2017/3/3.
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        System.out.println("1111111111111111111111111111");
+        StringBuffer stringBuffer = new StringBuffer();
+    }
+}
